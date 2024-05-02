60bf11e14a29390c4e2f98d0d240c1708e92dc83
Francisco Villanueva <154241922+RaFranVJ@users.noreply.github.com>
2024-05-02 08:58:47 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,12 @@
+/** <b> [Modulo del proyecto] </b>
+
+<br><br> Permite el acceso a las implementaciones hechas en <c>FranUtils</c>
+
+<br><br> También se encarga de exportar los ejercicios para que sean visibles desde otros paquetes  */
+
+module Programación2
+{
+// Definir las dependencias aquí
+
+requires FranUtilsMod;
+}
